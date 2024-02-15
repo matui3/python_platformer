816d1a77564ba83b26ed48ff8cb2468950bc9fc3
Jon Formantes <36646177+matui3@users.noreply.github.com>
2024-02-15 01:03:13 -0800

    updates made

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity
 class Game:
     def __init__(self):
 
@@ -8,49 +10,55 @@ class Game:
 
         pygame.display.set_caption('ninja game')
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        self.set_colorkey((0, 0, 0))
-
-        self.img_pos = [160, 260]
         self.movement = [False, False]
 
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png')
+        }
+
         self.collision_area = pygame.Rect(50, 50, 300, 50)
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
-            
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+            # img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            # # pygame.Rect(*self.img_pos, *self.img.get_size())
+            # if img_r.colliderect(self.collision_area):
+            #     pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            # else:
+            #     pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
 
-            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            # pygame.Rect(*self.img_pos, *self.img.get_size())
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
-
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            self.screen.blit(self.img, self.img_pos)
+            # self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            # self.screen.blit(self.img, self.img_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
-                        self.moveent[1] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
                 
-
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
             pygame.display.update()
             self.clock.tick(60)
 
