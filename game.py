a0af9752b6874c8ca8ec4332f0c773f970ea9a0f
Jon Formantes <36646177+matui3@users.noreply.github.com>
2024-06-26 17:33:09 -0700

    Some updates

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame
 
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 class Game:
     def __init__(self):
 
@@ -27,21 +28,16 @@ class Game:
         self.collision_area = pygame.Rect(50, 50, 300, 50)
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
+        self.tilemap = Tilemap(self, tile_size=16)
+
     def run(self):
         while True:
             self.display.fill((14, 219, 248))
 
+            self.tilemap.render(self.display)
+
             self.player.update((self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
-            # img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            # # pygame.Rect(*self.img_pos, *self.img.get_size())
-            # if img_r.colliderect(self.collision_area):
-            #     pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            # else:
-            #     pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
-
-            # self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            # self.screen.blit(self.img, self.img_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- scripts/entities.py
+++ scripts/entities.py
@@ -10,6 +10,9 @@ class PhysicsEntity:
 
     def update(self, movement=(0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1) 
+
         self.pos[0] += frame_movement[0]
         self.pos[1] += frame_movement[1]
 
